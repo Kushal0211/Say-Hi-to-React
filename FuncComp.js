373a7e1e3d17ffa1dcb6cd4a099aca1eb5470a94
Kushal Pandita <kushalpandita007@gmail.com>
2023-09-03 15:53:16 +0530

    Conclude the Learnings on Functional Components

--- FuncComp.js
+++ FuncComp.js
@@ -37,20 +37,56 @@ const HeadingComponent = () => {
 
 // we can also insert Component inside Component just like this
 
-const Title = () => (
-    <h1 className="heading2">I am also one Heading </h1>
-);
+const elem = <span> I am a span here...</span>;
+// -------> Above line is a JSX (React Element)
+
+// const Title =  (
+
+//     <h1 className="heading2">
+//         {elem}
+//         I am also one Heading 
+//     </h1>
+// );
+
+
+
+
+const number = 100+200;
 
 const HeadComp = () => (
 
     <div id="Func1">
+        <h2> {number} </h2>
         <img src="pic2.jpg" alt="Image is Loading"/>
         <h1 className="heading1">Namaste Engineer Sahab. I am Learning Functional Component </h1>
-        <Title/>
+
+        
     </div>
 
 );
 
+
+// Title is also a JSX 
+
+
+const Title =  (
+
+    // <h1 className="heading2">
+    //     {elem}
+    //     I am also one Heading 
+    // </h1>
+
+    <div id="div1">
+        <HeadComp></HeadComp>
+        <HeadComp/>
+    </div>
+   
+);
+
+// ---> Difference i encountered or observed is that if we are using jsx inside functional component or inside any other component , it can be written inside {} curly braces. 
+
+// -----> But But But if we are insering or using Functional Component it can be written only inside Angular brackets like <HeadComp/>
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadComp/>); 
+root.render(Title); 
